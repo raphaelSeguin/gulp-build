527e98b21e8539f1363e1584e084696f233800b2
raphaelSeguin <raphaelseguin@gmail.com>
2018-04-17 17:06:15 +0200

    temporary final version

--- gulpfile.js
+++ gulpfile.js
@@ -14,122 +14,75 @@ const              gulpif = require('gulp-if');
 
 const                 del = require('del');
 const                http = require('http');
+const            lazypipe = require('lazypipe');
 
-/**
- * As a developer, I should be able to run the gulp scripts command at the command line to concatenate, minify, and copy all of the project’s JavaScript files into an all.min.js file that is then copied to the dist/scripts folder.
- */
-
-// * * * * * * * * * * * * * * * * * * * * * * * * * * * * * concat-js
-gulp.task('concat-js', function() {
-    return gulp.src(["./jquery-3.3.1.min.js", "./js/circle/autogrow.js", "./js/circle/circle.js", "global.js"])
+// * * * * * * * * * * * * * * * * * * * * * * * * * * * * * scripts
+gulp.task('scripts', function() {
+    return gulp.src(["./js/jquery-3.3.1.min.js", "./js/circle/autogrow.js", "./js/circle/circle.js", "./js/global.js"])
         .pipe( maps.init() )
         .pipe( concat("all.js") )
-        .pipe( maps.write() )
-        .pipe( gulp.dest("./js") )
-        ;
-});
-
-// * * * * * * * * * * * * * * * * * * * * * * * * * * * * * minify-js
-gulp.task('minify-js',['concat-js'], function() {
-    return gulp.src("./js/all.js")
         .pipe( uglify() )
         .pipe( rename("all.min.js") )
+        .pipe( maps.write("./") )
         .pipe( gulp.dest("./dist/scripts") )
-        ;
 });
 
-// * * * * * * * * * * * * * * * * * * * * * * * * * * * * * move-js-map
-gulp.task('move-js-map',['concat-js'], function() {
-    return gulp.src("./js/all.js.map")
-        .pipe( rename("all.min.js.map") )
-        .pipe( gulp.dest("./dist/scripts") )
-        ;
-})
-// * * * * * * * * * * * * * * * * * * * * * * * * * * * * * move-sass-map
-gulp.task('move-sass-map', function() {
-    return gulp.src("./sass/*.map")
-        .pipe( rename("all.min.css.map") )
-        .pipe( gulp.dest("./dist/styles/") )
+// * * * * * * * * * * * * * * * * * * * * * * * * * * * * * styles
+gulp.task('styles', function() {
+    return gulp.src("./sass/global.scss")
+        .pipe( maps.init() )
+        .pipe( sass() )
+        .pipe( minifycss() )
+        .pipe( rename("all.min.css") )
+        .pipe( maps.write("./") )
+        .pipe( gulp.dest("./dist/styles") )
+});
+
+// 
+
+gulp.task('pre-useref-sass', ['clean'], function() {
+    return gulp.src("./sass/global.scss")
+        .pipe( sass() )
+        .pipe( rename("global.css") )
+        .pipe( gulp.dest("./sass"))
 })
 
-// * * * * * * * * * * * * * * * * * * * * * * * * * * * * * html
-gulp.task('html',['compile-sass'], function () {
+gulp.task('pre-useref-js', function() {
+    return gulp.src(["./js/jquery-3.3.1.min.js", "./js/circle/autogrow.js", "./js/circle/circle.js", "./js/global.js"])
+        .pipe( concat("all.js") )
+        .pipe( gulp.dest("./js"))
+})
+
+// * * * * * * * * * * * * * * * * * * * * * * * * * * * * * useref
+gulp.task('useref',['pre-useref-sass', 'pre-useref-js' ], function () {
     return gulp.src('./index.html')
-        .pipe( useref() )
+        .pipe( useref({}, lazypipe().pipe(maps.init, { loadMaps: true })) )
+        .pipe( maps.write("./") )
         .pipe( gulpif('*.js', uglify()) )
         .pipe( gulpif('*.css', minifycss()) )
         .pipe( gulp.dest('dist') );
 });
 
-// * * * * * * * * * * * * * * * * * * * * * * * * * * * * * scripts
-gulp.task('scripts', ['minify-js', 'move-js-map'], function() {
-    return console.log('scripts havec been concatenated and minified in folder dist/scripts')
-});
-
-/**
- * As a developer, I should be able to run the gulp styles command at the command line to compile the project’s SCSS files into CSS, then concatenate and minify into an all.min.css file that is then copied to the dist/styles folder.
- */
-
-// compile sass and minify css
-// * * * * * * * * * * * * * * * * * * * * * * * * * * * * * compile-sass
-
-gulp.task('compile-sass', function() {
-    return gulp.src("./sass/global.scss")
-        .pipe( maps.init() )
-        .pipe( sass() )
-        .pipe( minifycss() )
-        .pipe( rename("global.css"))
-        .pipe( maps.write("./") )
-        .pipe( gulp.dest("./sass") )
-})
-// * * * * * * * * * * * * * * * * * * * * * * * * * * * * * styles
-gulp.task('styles',['compile-sass'], function() {
-    return gulp.src("./sass/global.css*")
-        .pipe( gulp.dest("./dist/styles") )
-        ;
-});
-
-/**
- * As a developer, I should be able to run the gulp images command at the command line to optimize the size of the project’s JPEG and PNG files, and then copy those optimized images to the dist/content folder.
- */
-
 // * * * * * * * * * * * * * * * * * * * * * * * * * * * * * images
 gulp.task('images', function() {
     return gulp.src("./images/*.{jpg,png}")
         .pipe( smushit() )
-        .pipe( gulp.dest("./dist/images"))
+        .pipe( gulp.dest("./dist/content"))
 });
-
-/**
- * As a developer, I should be able to run the gulp clean command at the command line to delete all of the files and folders in the dist folder.
- */
 
  // * * * * * * * * * * * * * * * * * * * * * * * * * * * * * clean
 gulp.task('clean', function() {
     return del(["./dist", "./js/all.js*", "./sass/*.css*", "./js/all.js*"]);
-})
-
-/**
- * As a developer, I should be able to run the gulp build command at the command line to run the clean, scripts, styles, and images tasks with confidence that the clean task completes before the other commands.
- */
-
-// * * * * * * * * * * * * * * * * * * * * * * * * * * * * * build
-gulp.task('build', ['clean'], function() {
-    gulp.start(['html', 'images']);
-    return gulp.start(['move-sass-map', 'move-js-map']);
 });
 
-/**
- * As a developer, I should be able to run the gulp command at the command line to run the build task and serve my project using a local web server.
- */
+// * * * * * * * * * * * * * * * * * * * * * * * * * * * * * build
+gulp.task('build', ['clean', 'useref', 'images'], function() {
+    return console.log('\nbuild done!\n');
+});
 
-/**
- * As a developer, when I run the default gulp command, it should continuously watch for changes to any .scss file in my project. When there is a change to one of the .scss files, the gulp styles command is run and the files are compiled, concatenated, and minified to the dist folder. My project should then reload in the browser, displaying the changes.
- */
-
- // * * * * * * * * * * * * * * * * * * * * * * * * * * * * * default
+// * * * * * * * * * * * * * * * * * * * * * * * * * * * * * default
 gulp.task('default',['build', 'watch-sass'], function() {
-    gulp.src("./dist")
+    return gulp.src("./dist")
         .pipe(
             webserver({
                 livereload: true,
@@ -140,19 +93,12 @@ gulp.task('default',['build', 'watch-sass'], function() {
 });
 
 // * * * * * * * * * * * * * * * * * * * * * * * * * * * * * watch-sass
-gulp.task('watch-sass', function() {
-    gulp.watch("sass/**/*.s*ss", ['styles']);
-})
+gulp.task('watch-sass', ['build'], function() {
+    return gulp.watch("sass/**/*.s*ss", ['styles']);
+});
 
-// * * * * * * * * * * * * * * * * * * * * * * * * * * * * * serve
-gulp.task('serve', function() {
-    gulp.src("./dist")
-        .pipe(
-            webserver({
-                livereload: true,
-                open: true
-            })
-        )
-        ;
-})
-
+/*
+styles
+scripts
+images
+*/
